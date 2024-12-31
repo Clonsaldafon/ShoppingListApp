6a7477bf4a8eda855b381676586f498098a3a4ce
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2024-12-31 16:05:44 +0500

    Update navigation and add groups screen

--- app/src/main/java/ru/clonsaldafon/shoppinglistapp/presentation/NavGraph.kt
+++ app/src/main/java/ru/clonsaldafon/shoppinglistapp/presentation/NavGraph.kt
@@ -1,26 +1,54 @@
 package ru.clonsaldafon.shoppinglistapp.presentation
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import ru.clonsaldafon.shoppinglistapp.R
+import ru.clonsaldafon.shoppinglistapp.presentation.groups.GroupsScreen
 import ru.clonsaldafon.shoppinglistapp.presentation.login.LogInScreen
 import ru.clonsaldafon.shoppinglistapp.presentation.signup.SignUpScreen
 
 sealed class Routes(val route: String) {
     data object SignUp : Routes("signup")
     data object LogIn : Routes("login")
+    data object Groups : Routes("groups")
+    data object Profile : Routes("profile")
 }
 
 @Composable
 fun NavGraph(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    navController: NavHostController
 ) {
    NavHost(
        navController = navController,
-       startDestination = Routes.SignUp.route
+       startDestination = Routes.Groups.route,
+       enterTransition = {
+           EnterTransition.None
+       },
+       exitTransition = {
+           ExitTransition.None
+       }
    ) {
        composable(route = Routes.SignUp.route) {
            SignUpScreen(
@@ -31,6 +59,20 @@ fun NavGraph(
 
        composable(route = Routes.LogIn.route) {
            LogInScreen(
+               modifier = modifier,
+               navController = navController
+           )
+       }
+
+       composable(route = Routes.Groups.route) {
+           GroupsScreen(
+               modifier = modifier,
+               navController = navController
+           )
+       }
+
+       composable(route = Routes.Profile.route) {
+           ProfileScreen(
                modifier = modifier,
                navController = navController
            )
---
+++ app/src/main/java/ru/clonsaldafon/shoppinglistapp/presentation/groups/GroupsScreen.kt
@@ -0,0 +1,307 @@
+package ru.clonsaldafon.shoppinglistapp.presentation.groups
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import ru.clonsaldafon.shoppinglistapp.R
+import ru.clonsaldafon.shoppinglistapp.presentation.Routes
+import ru.clonsaldafon.shoppinglistapp.ui.theme.DarkGreen
+import ru.clonsaldafon.shoppinglistapp.ui.theme.DarkOrange
+import ru.clonsaldafon.shoppinglistapp.ui.theme.Green
+import ru.clonsaldafon.shoppinglistapp.ui.theme.Orange
+import ru.clonsaldafon.shoppinglistapp.ui.theme.White
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GroupsScreen(
+    modifier: Modifier = Modifier,
+    navController: NavHostController? = null
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .shadow(
+                        elevation = 16.dp,
+                        shape = RoundedCornerShape(
+                            bottomStart = 30.dp,
+                            bottomEnd = 30.dp
+                        )
+                    )
+                    .background(
+                        color = Green,
+                        shape = RoundedCornerShape(
+                            bottomStart = 30.dp,
+                            bottomEnd = 30.dp
+                        )
+                    ),
+                windowInsets = WindowInsets(
+                    top = 20.dp,
+                    bottom = 20.dp
+                ),
+                colors = topAppBarColors(
+                    containerColor = Green,
+                    titleContentColor = White
+                ),
+                title = {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "Группы",
+                        style = TextStyle(
+                            fontSize = 40.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center
+                        )
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                modifier = modifier
+                    .clip(
+                        shape = RoundedCornerShape(
+                            topStart = 30.dp,
+                            topEnd = 30.dp
+                        )
+                    ),
+                containerColor = Green,
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    IconButton(
+                        modifier = Modifier
+                            .width(50.dp)
+                            .height(50.dp),
+                        onClick = {}
+                    ) {
+                        Image(
+                            modifier = Modifier
+                                .width(40.dp)
+                                .height(40.dp),
+                            bitmap = ImageBitmap.imageResource(R.drawable.groups_selected),
+                            contentDescription = "Группы"
+                        )
+                    }
+
+                    IconButton(
+                        modifier = Modifier
+                            .width(50.dp)
+                            .height(50.dp),
+                        onClick = {
+                            navController?.navigate(Routes.Profile.route)
+                        }
+                    ) {
+                        Image(
+                            modifier = Modifier
+                                .width(40.dp)
+                                .height(40.dp),
+                            bitmap = ImageBitmap.imageResource(R.drawable.profile),
+                            contentDescription = "Профиль"
+                        )
+                    }
+                }
+            }
+        },
+        floatingActionButton = {
+            IconButton(
+                modifier = Modifier
+                    .width(60.dp)
+                    .height(60.dp)
+                    .shadow(
+                        elevation = 4.dp,
+                        shape = RoundedCornerShape(60.dp)
+                    )
+                    .background(
+                        color = DarkOrange,
+                        shape = RoundedCornerShape(60.dp)
+                    ),
+                onClick = {}
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .width(35.dp)
+                        .height(35.dp),
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = null,
+                    tint = White
+                )
+            }
+        },
+        content = { innerPadding ->
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .background(
+                        color = White
+                    )
+                    .padding(innerPadding)
+            ) {
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxSize(),
+//                verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            ) {
+//                Text(
+//                    text = "Вы еще не\nсостоите в группе",
+//                    style = TextStyle(
+//                        color = DarkGreen,
+//                        fontSize = 32.sp,
+//                        textAlign = TextAlign.Center
+//                    )
+//                )
+//            }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            horizontal = 40.dp,
+                            vertical = 20.dp
+                        ),
+                    verticalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    GroupItem(
+                        title = "Семья",
+                        members = 4
+                    )
+
+                    GroupItem(
+                        title = "Друзья",
+                        members = 4
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun GroupItem(
+    title: String,
+    members: Int
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(
+                horizontal = 20.dp,
+                vertical = 17.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp),
+                imageVector = Icons.Filled.Star,
+                contentDescription = null,
+                tint = Color.LightGray
+            )
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(14.dp)
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(
+                        color = DarkGreen,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+
+                Text(
+                    text = "$members чел.",
+                    style = TextStyle(
+                        color = Green,
+                        fontSize = 20.sp
+                    )
+                )
+            }
+        }
+
+        IconButton(
+            modifier = Modifier
+                .height(30.dp)
+                .width(30.dp),
+            onClick = {}
+        ) {
+            Icon(
+                modifier = Modifier
+                    .height(30.dp)
+                    .width(30.dp),
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = null,
+                tint = Orange
+            )
+        }
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Composable
+fun MyPreview() {
+    GroupsScreen()
+}
